f7601c88ceeaad7167303ef4e7705ac70c029e9b
JesseRatt <=>
2022-09-27 05:32:27 -0500

    Add sfx files.

---
+++ src/audio/sfx/pause.h
@@ -0,0 +1,18 @@
+/*
+
+	SFX_0D
+
+	Sound Effect File.
+	
+	Info:
+		Length			:	9
+		Priority		:	2
+		Channels used	:	Noise channel
+		SGB Support		:	No
+*/
+
+#ifndef __SFX_0D_h_INCLUDE
+#define __SFX_0D_h_INCLUDE
+#define CBTFX_PLAY_SFX_0D CBTFX_init(&SFX_0D[0])
+extern const unsigned char SFX_0D[];
+#endif
