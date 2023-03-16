ba659646d011ef6348759b51163df8573ac65afe
bbbbbr <bbbbbr@users.noreply.github.com>
2023-03-15 21:58:01 -0700

    Splash Screen: Move High Score 1 tile to the right

--- src/splash_screen.h
+++ src/splash_screen.h
@@ -3,7 +3,7 @@
 
 #define SPLASH_LOGO_WIN_X 0u
 #define SPLASH_LOGO_WIN_Y 0u
-#define SPLASH_LOGO_WIN_SCORE_X 14u
+#define SPLASH_LOGO_WIN_SCORE_X 15u
 #define SPLASH_LOGO_WIN_SCORE_Y 10u
 
 uint8_t splash_intro_run(uint8_t);
