d911da77ac7d15e9711e09e52a182bb216c3376a
bbbbbr <bbbbbr@users.noreply.github.com>
2023-04-09 15:34:06 -0700

    Flip Reveal FX: use div, remove LUT (smaller code) - vbl handler duration seems ok with divide - resulting code is smaller with division instead of the stretch increment LUT - code also returns to being configurable (increment speed/size)

--- src/help_screen_effect.c
+++ src/help_screen_effect.c
@@ -21,14 +21,12 @@
 
 #define EFFECT_HIDE_LINE      10u // Map scanline to use as Blank/Hidden
 
-// The speed the flip reveal effect visible region increases
+// The speed the flip reveal effect visible region increases (1 = slow, larger = faster/coarser)
 #define EFFECT_DIR_UP          (uint8_t)-4
 #define EFFECT_DIR_DOWN        4u
 #define EFFECT_DIR_NONE        0u
 #define EFFECT_TRUNC_TO_INC(VAL)  (VAL - (VAL % EFFECT_DIR_DOWN)) // Ensures increments and target scanlines are even multiple of increment size
 
-
-#define EFFECT_LUT_SHIFT       2   // (1 << 2) to match increment by 4
 #define EFFECT_CYCLES          3u  // Total number of flips to perform
 #define EFFECT_CYCLE_INC_SIZE  (EFFECT_TRUNC_TO_INC((DEVICE_SCREEN_PX_HEIGHT / 2u) / EFFECT_CYCLES))
 
@@ -44,16 +42,6 @@
 #endif
 
 #define CALC_SCY_TO_SHOW_MAP_Y_AT_SCANLINE_N(map_y_to_show, scanline_to_display_at) (map_y_to_show - scanline_to_display_at)
-
-
-// Increment by 1
-// Lookup: [effect_y_line]
-// static const uint16_t render_increment_LUT[] = { 256u, 259u, 263u, 267u, 271u, 275u, 279u, 283u, 288u, 292u, 297u, 302u, 307u, 312u, 317u, 323u, 329u, 335u, 341u, 347u, 354u, 361u, 368u, 376u, 384u, 392u, 400u, 409u, 418u, 428u, 438u, 449u, 460u, 472u, 485u, 498u, 512u, 526u, 542u, 558u, 576u, 594u, 614u, 635u, 658u, 682u, 708u, 737u, 768u, 801u, 837u, 877u, 921u, 970u, 1024u, 1084u, 1152u, 1228u, 1316u, 1417u, 1536u, 1675u, 1843u, 2048u, 2304u, 2633u, 3072u, 3686u, 4608u, 6144u, 9216u, 18432u};
-
-// Increment by 4
-// Lookup: [effect_y_line >> 2]
-static const uint16_t render_increment_LUT[] = {256u, 271u, 288u, 307u, 329u, 354u, 384u, 418u, 460u, 512u, 576u, 658u, 768u, 921u, 1152u, 1536u, 2304u, 4608u, 36864u};
-
 
 static uint16_t map_y_line_to_display;
 static uint16_t map_y_line_inc;
@@ -102,7 +90,7 @@ static void help_effect_vbl_isr(void) {
 
     // Calculate bottom of effect and increment per render scanline
     effect_y_line_mirror = DEVICE_SCREEN_PX_HEIGHT - effect_y_line;
-    map_y_line_inc = render_increment_LUT[effect_y_line >> EFFECT_LUT_SHIFT];
+    map_y_line_inc = (DEVICE_SCREEN_PX_HEIGHT << 8) / (uint16_t)(effect_y_line_mirror - effect_y_line);
 
     // Set up values used by scanline ISR during the frame for the stretched render area
     if (!effect_vflip) {
